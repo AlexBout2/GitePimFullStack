fae3ad3045554bb529a7470ddf57993811ce1705
Alexis Boutin <a.boutin@cci-formation.nc>
2025-05-21 22:00:38 +1100

    Tweak de resources/views/components/form-errors.blade.php pour des messages d'erreur cleint plus soft

--- resources/views/components/form-errors.blade.php
+++ resources/views/components/form-errors.blade.php
@@ -1,12 +1,12 @@
 @props([
     'showAll' => true,
     'fields' => [], // Champs spécifiques pour lesquels afficher les erreurs
-    'title' => 'Veuillez corriger les erreurs suivantes:',
+    'title' => 'Après syncronisation, il s\'emble il y avoir un soucis',
 ])
 
 @if ($errors->any())
-    <div class="alert alert-danger">
-        <p class="mb-2">{{ $title }}</p>
+    <div class="alert alert-info">
+        <p class="mb-2 text-center">{{ $title }}</p>
         <ul class="mb-0">
             @if ($showAll)
                 @foreach ($errors->all() as $error)
